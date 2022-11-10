76d792c5544e45218530aab2d2761a8c2e39da04
Andrew LC <smartandrew2003@gmail.com>
2022-11-10 21:13:54 +0530

    Second Commit

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com']
   },
-  env: {
-    AUTH_KEY: 'secret_npYubty5LrYRwmF8td94sTk5T6d1IpUBP9wizHzi1Zn',
-    DATABASE_ID: 'dcf901c081424e1e8cc0a7230add4381'
-  }
 }
 
 module.exports = nextConfig
